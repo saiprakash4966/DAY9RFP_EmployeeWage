2e62c3be607ed2aaf4a15f83b9f0eda8b4362237
unknown <saiprakashbeemari@gmail.com>
2022-02-23 23:31:06 +0530

    Employee wage UC2

--- EmpWageOOPs.java
+++ EmpWageOOPs.java
@@ -2,33 +2,60 @@ package com.bl;
 
 public class EmpWageOOPs
 {
-		//Method to check  employee present or not
-		private static void checkEmpPresentOrNot(int is_fulltime)
-		{
-			//  number for 0 for absent 1 for present
-			int empCheck=(int)Math.floor(Math.random()*10) % 2;
-			//System.out.println(empCheck);
+	final static int emp_rate_per_hour=40;
 
-			//if condition to check employee is present for the input is_fulltime=1
-			if(empCheck==is_fulltime)
-			{
-				System.out.println("Employee is Present");
-			}
-			//else condition to check employee is absent for the input is_fulltime=0
-			else
-				System.out.println("Employee is Absent");
-		}
+    //Method to Generate Random NUmber
+	private static int getRandomNumber(int emp)
+	{
+		//Generate random number for 0 for absent 1 for present
+				int empCheck=(int)Math.floor(Math.random()*10) % 2;
+				//System.out.println(empCheck);
+				return empCheck;
 
-
-
-
-		public static void main(String[] args)
-		{
-			//declaring constants
-			int is_fulltime=1;
-			checkEmpPresentOrNot(is_fulltime);
-
-		}
-}
+	}
+	/*
+	 * //Method to check whether employee present or not private static void
+	 * checkEmpPresentOrNot(int is_fulltime) { int empCheck=0;
+	 * empCheck=getRandomNumber(empCheck);
+	 * 
+	 * 
+	 * //if condition to check employee is present for the input is_fulltime=1
+	 * if(empCheck==is_fulltime) { System.out.println("Employee is Present"); }
+	 * //else condition to check employee is absent for the input is_fulltime=0 else
+	 * System.out.println("Employee is Absent"); }
+	 */
 
 
+	private static void calculateDailyWage(int is_fulltime) {
+		//constants
+
+
+		//variables
+		int empHrs=0;
+		int empWage=0;
+		int empCheck=0;
+
+		empCheck=getRandomNumber(empCheck);
+		System.out.println(empCheck);
+		if(empCheck==is_fulltime) 
+			empHrs=8;
+		else
+			empHrs=4;
+
+		empWage= emp_rate_per_hour * empHrs;
+
+		System.out.println("Emp Wage is "+empWage);
+
+	}
+
+
+	public static void main(String[] args)
+	{
+		//declaring constants
+		int is_fulltime=1;
+		int empCheck=0;
+		getRandomNumber(empCheck);
+		//checkEmpPresentOrNot(is_fulltime);
+		calculateDailyWage(is_fulltime);
+
+	}}
