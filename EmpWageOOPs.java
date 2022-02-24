8630d0e6b39919042282a4ee1068086ebd58308a
unknown <saiprakashbeemari@gmail.com>
2022-02-24 16:10:22 +0530

    Employee wage

--- EmpWageOOPs.java
+++ EmpWageOOPs.java
@@ -2,60 +2,50 @@ package com.bl;
 
 public class EmpWageOOPs
 {
-	final static int emp_rate_per_hour=40;
+	//constants
+	public static final int EMP_RATE_PER_HOUR=20;
+	//method to calculate daily employee wage of the employee
+	private static void calculateDailyWage(int is_fulltime) 
+	{}
 
-    //Method to Generate Random NUmber
-	private static int getRandomNumber(int emp)
-	{
-		//Generate random number for 0 for absent 1 for present
-				int empCheck=(int)Math.floor(Math.random()*10) % 2;
-				//System.out.println(empCheck);
-				return empCheck;
-
-	}
-	/*
-	 * //Method to check whether employee present or not private static void
-	 * checkEmpPresentOrNot(int is_fulltime) { int empCheck=0;
-	 * empCheck=getRandomNumber(empCheck);
-	 * 
-	 * 
-	 * //if condition to check employee is present for the input is_fulltime=1
-	 * if(empCheck==is_fulltime) { System.out.println("Employee is Present"); }
-	 * //else condition to check employee is absent for the input is_fulltime=0 else
-	 * System.out.println("Employee is Absent"); }
-	 */
-
-
-	private static void calculateDailyWage(int is_fulltime) {
-		//constants
-
+	private static void calculateDailyWage() {
+		
 
 		//variables
+		final int fulltime=2;
+		final int parttime=1;
 		int empHrs=0;
 		int empWage=0;
 		int empCheck=0;
 
-		empCheck=getRandomNumber(empCheck);
-		System.out.println(empCheck);
+		empCheck=(int)Math.floor(Math.random()*10) % 2;
+		empCheck=(int)Math.floor(Math.random()*10) % 3 ;
+		int is_fulltime = 0;
+		//System.out.println(empCheck);
 		if(empCheck==is_fulltime) 
 			empHrs=8;
 		else
 			empHrs=4;
+				if(empCheck==fulltime) 
+					empHrs=8;
+				else if(empCheck==parttime)
+					empHrs=4;
+				else
+					empHrs=0;
 
-		empWage= emp_rate_per_hour * empHrs;
+		empWage= EMP_RATE_PER_HOUR * empHrs;
+				empWage= EMP_RATE_PER_HOUR * empHrs;
 
 		System.out.println("Emp Wage is "+empWage);
+				
 
 	}
 
-
-	public static void main(String[] args)
+	public static void main(String[] args) 
 	{
-		//declaring constants
 		int is_fulltime=1;
-		int empCheck=0;
-		getRandomNumber(empCheck);
-		//checkEmpPresentOrNot(is_fulltime);
 		calculateDailyWage(is_fulltime);
 
-	}}
+		calculateDailyWage();
+	}
+}
